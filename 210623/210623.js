18da12f2f4b78ccf5bd75c19ce6168bfecd5bde7
wkjung0624 <skyship36@gmail.com>
2021-06-23 23:07:09 +0900

    Add click event to vote button

---
+++ 210623/210623.js
@@ -0,0 +1,9 @@
+{
+    function isVotted(vState){       
+        
+        alert(vState);
+        vState == true ? alert("isVotted 호출 - Like 버튼 클릭") : alert("isVotted 호출, Hate 버튼 클릭됨");
+        
+    }
+    
+}
